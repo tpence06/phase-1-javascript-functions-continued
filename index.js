327d7ed1333559caeac123cb8441926e9b2d3360
tpence06 <tpence06@hotmail.com>
2023-02-22 17:41:46 -0700

    pass tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity) {
+    console.log(`This Saturday, I want to ${activity}!`);
+    if (activity == null) 
+        activity = 'roller-skate';
+    return(`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(work) {
+    console.log(`This Monday, I will ${work}.`)
+    if (work == null)
+        work = 'go to the office';
+    return(`This Monday, I will ${work}.`)
+}
+let wrapAdjective = function(style="*") {
+    return function(adj="special") {
+      return `You are ${style}${adj}${style}!`
+    }
+  }
